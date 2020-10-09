720bc701ff7207eb79a0cc71ec9e953dfdc2e830
Frederico de Angelis Reis <angelisreisbiof@gmail.com>
2020-10-09 18:42:33 -0300

    contador na apresentação ok

--- scripts/main_flipdown.js
+++ scripts/main_flipdown.js
@@ -3,8 +3,10 @@ document.addEventListener('DOMContentLoaded', () => {
   // Unix timestamp (in seconds) to count down to
   var twoDaysFromNow = (new Date().getTime() / 1000) + (86400 * 2) + 1;
 
+  var event_date = 1604396400
+
   // Set up FlipDown
-  var flipdown = new FlipDown(twoDaysFromNow)
+  var flipdown = new FlipDown(event_date)
 
     // Start the countdown
     .start()
@@ -17,12 +19,15 @@ document.addEventListener('DOMContentLoaded', () => {
   // Toggle theme
   var interval = setInterval(() => {
     let body = document.body;
-    body.classList.toggle('light-theme');
-    body.querySelector('#flipdown').classList.toggle('flipdown__theme-dark');
-    body.querySelector('#flipdown').classList.toggle('flipdown__theme-light');
+
+    body.classList.toggle('dark-theme');
+
+    // body.querySelector('#flipdown').classList.toggle('flipdown__theme-dark');
+    
+    // body.querySelector('#flipdown').classList.toggle('flipdown__theme-light');
+
   }, 5000);
 
   // Show version number
-  var ver = document.getElementById('ver');
-  ver.innerHTML = flipdown.version;
+  
 });
